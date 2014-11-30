c9582c8b63b8964add2b87a09c44324b1e60a548
nettolt <nettolt@users.noreply.github.com>
2014-11-30 14:24:02 -0700

    Open file object with 'rU'
    fo = open(filepath_or_fileobj, 'rU')

--- csv2sqlite.py
+++ csv2sqlite.py
@@ -9,7 +9,7 @@ import sqlite3
 
 def convert(filepath_or_fileobj, dbpath, table='data'):
     if isinstance(filepath_or_fileobj, basestring):
-        fo = open(filepath_or_fileobj)
+        fo = open(filepath_or_fileobj, 'rU')
     else:
         fo = filepath_or_fileobj
     reader = csv.reader(fo)
